e5bca4dbddb7e3c259311fc9d77c9fd6d2c1d87a
Maqsud2010 <maqsudjonmamanazarov3@gmail.com>
2023-09-28 21:21:56 +0500

    uy ishi

--- app.js
+++ app.js
@@ -20,3 +20,34 @@ if(selectLang === "uzb"){
         `)
     }
 }
+
+let wordsrus ="Я́блоко — сочный плод яблони, который употребляется в пищу в свежем и запеченном виде, служит сырьём в кулинарии и для приготовления напитков. Наибольшее распространение получила яблоня домашняя, реже выращивают яблоню сливолистную. Размер красных, зелёных, жёлтых или оранжевых шаровидных плодов 5—13 см в диаметре. Происходит из Центральной Азии, где до сих пор произрастает дикорастущий предок яблони домашней — яблоня Сиверса[1]. На сегодняшний день существует множество сортов этого вида яблони, произрастающих в различных климатических условиях. По времени созревания отличают летние, осенние и зимние сорта, более поздние сорта отличаются хорошей стойкостью."
+
+
+if(selectLang === "rus"){
+    let count=+prompt("СКОЛЬКО СЛОВ ВАМ НУЖНО")
+    let wordsArrayrus=wordsrus.split(" ")
+
+
+
+    for (let i=0; i < count; i++){
+        let random = Math.floor(Math.random() * wordsArrayrus.length)
+        document.write(`
+        <span>${wordsArrayrus[random]}</span>
+        `)
+    }
+}
+let wordseng ="An apple is a round, edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found. Apples have been grown for thousands of years in Asia and Europe and were introduced to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.Apples grown from seed tend to be very different from those of their parents, and the resultant fruit frequently lacks desired characteristics. Generally, apple cultivars are propagated by clonal grafting onto rootstocks. Apple trees grown without rootstocks tend to be larger and much slower to fruit after planting. Rootstocks are used to control the speed of growth and the size of the resulting tree, allowing for easier harvesting.There are more than 7,500 cultivars of apples. Different cultivars are bred for various tastes and uses, including cooking, eating raw, and cider production. Trees and fruit are prone to fungal, bacterial, and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit's genome was sequenced as part of research on disease control and selective breeding in apple production."
+if(selectLang === "eng"){
+    let count=+prompt("HOW MANY WORDS DO YOU NEED")
+    let wordsArrayeng=wordseng.split(" ")
+
+
+
+    for (let i=0; i < count; i++){
+        let random = Math.floor(Math.random() * wordsArrayeng.length)
+        document.write(`
+        <span>${wordsArrayeng[random]}</span>
+        `)
+    }
+}
